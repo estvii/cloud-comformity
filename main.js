6462fb37278d02ec989a08d43ec2a6e26cd2ff5e
Steven Vi <visteven00@gmail.com>
2019-06-21 19:45:14 +1000

    added relationship between rules

--- main.js
+++ main.js
@@ -1,34 +1,67 @@
 const serviceBlocks = document.getElementById('service-blocks');
+const serviceBlocks2 = document.getElementById('service-blocks2');
+
+const serviceBlockGenerator = (item) => {
+    const rules = item.relationships.rules.data
+
+    // create a div class for a service
+    const serviceBlock = document.createElement('div');
+    serviceBlock.setAttribute('class', 'service-block');
+
+    // Creating content i.e children for the service block div
+    const h3 = document.createElement('h3');
+    h3.textContent = item.attributes.name;
+
+    // looping through the array of rules and appending it to the ul div
+    const ul = document.createElement('ul')
+    rules.map(rule => {
+        const li = document.createElement('li');
+        li.innerHTML = rule.id;
+        ul.appendChild(li);
+    })
+
+    // Appending content to service-block
+    serviceBlock.appendChild(h3);
+    serviceBlock.appendChild(ul);
+
+    // Attach the service-block div to the parent  div service blocks
+    // Check if service title is Budgets, then appends it to a different div
+    if (serviceBlock.childNodes[0].innerHTML == 'Budgets') {
+        serviceBlocks2.appendChild(serviceBlock);
+    } else {
+        serviceBlocks.appendChild(serviceBlock);
+    }
+
+
+};
+
+serviceRulesLinker = (included) => {
+
+    const all_lis = document.getElementsByTagName('li');
+
+    // looping through existing rules on page and overwriting them with included datas
+    for (let li of all_lis) {
+        included.map((item) => {
+            if (li.innerHTML === item.id) {
+                // console.log('matched'); should match 510 times because theres 510 rules
+                li.innerHTML = item.attributes.title
+            }
+        })
+    }
+};
+
 
 
 const fetchData = async () => {
     let response = await fetch('https://us-west-2.cloudconformity.com/v1/services');
     let object = await response.json();
+
     object.data.map(item => {
-        console.log(item.relationships.rules.data);
-
-        // create a div class for a service
-        const serviceBlock = document.createElement('div');
-        serviceBlock.setAttribute('class', 'service-block');
-
-        // Creating content i.e children for the service block div
-        const h3 = document.createElement('h3');
-        h3.textContent = item.attributes.name;
-
-        const ul = document.createElement('ul')
-        const li = document.createElement('li');
-        li.innerHTML = 'rules';
-        ul.appendChild(li);
-
-        // Appending content to service-block
-        serviceBlock.appendChild(h3);
-        serviceBlock.appendChild(ul);
-
-
-        // Attach the service-block div to the parent  div service blocks
-        serviceBlocks.appendChild(serviceBlock);
-
+        serviceBlockGenerator(item);
     });
+
+    serviceRulesLinker(object.included);
+
 
 }
 
